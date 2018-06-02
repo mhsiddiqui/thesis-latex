1b44359f0f41b9b28edda18d9ea1a33a4ab18e38
M Hassan Siddiqui <mhassan.eeng@gmail.com>
2018-06-02 18:10:24 +0500

    literature review completed

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import subprocess
 
 pdflatex = ["pdflatex", "-interaction=nonstopmode", "-file-line-error", "main"]
-biber = ["biber", "main"]
+biber = ["bibtex", "main"]
 
 print subprocess.Popen(pdflatex).communicate()   
 print subprocess.Popen(biber).communicate()   
