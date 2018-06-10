9bd620d5de1e49ae929f12d70a245db0f5190164
M Hassan Siddiqui <mhassan.eeng@gmail.com>
2018-06-10 16:34:15 +0500

    hindi issue resolved

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import subprocess
 
 pdflatex = ["xelatex", "-interaction=nonstopmode", "-file-line-error", "-shell-escape", "main"]
-biber = ["bibtex", "main"]
+biber = ["biber", "main"]
 
 print subprocess.Popen(pdflatex).communicate()   
 print subprocess.Popen(biber).communicate()   
